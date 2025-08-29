08d4e2ce9719c778c0346cc93facd00efc238bd1
WooJinHong <orolzleim@gmail.com>
2025-08-29 12:24:41 +0900

    1543(문자열)

---
+++ src/String/b_1157.java
@@ -0,0 +1,47 @@
+package String;
+
+import java.util.Scanner;
+
+public class b_1157 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        // count 배열 생성  ASCII 코드 (a~z) (1~26)  a = 65    A = 85
+        // 0 ~ 25
+        int[] countRepeatWord = new int[26];
+
+        // 입력 값을 char 배열로 치환
+        char[] splitWord = word.toCharArray();
+
+        // 입력 값 저장 missisipi
+        for(int i = 0; i < word.length(); i++){
+            // 입력 값이 소문자 일 때  예) 'b'-'a' = 9
+            if('a'<=splitWord[i] && splitWord[i]<='z')
+                countRepeatWord[splitWord[i]-'a']++;
+
+            // 입력 값이 대문자 일 때   예) 'B'-'A' = 1
+            else
+                countRepeatWord[splitWord[i]-'A']++;
+        }
+
+        // 첫번째 배열 max 변수에 저장
+        int max = -1;
+        char alphabet='?';
+        for(int i = 0; i < countRepeatWord.length; i++){
+            if(max < countRepeatWord[i]) {
+                max = countRepeatWord[i];
+                alphabet = (char)(i + 'A');
+            }
+            // 알파벳 최대 빈도수가 두개이상 같을때 '?' 출력
+            else if(max == countRepeatWord[i]){
+                alphabet = '?';
+            }
+        }
+
+        System.out.println(alphabet);
+    }
+}
---
+++ src/String/b_1543.java
@@ -0,0 +1,37 @@
+package String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b_1543 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 문서 입력 값 추출 (ababababa)
+        String doc = sc.nextLine();
+
+        // 단어 입력 값 추출 (aba)
+        String word = sc.nextLine();
+
+        int count =0;
+
+        // 1. 문서의 길이만큼 LOOP
+        for (int i = 0; i < doc.length(); i++){
+            // 1.1. 문서와 단어가 일치 시 count 증가
+            boolean matchCase = true;
+            for(int j = 0; j < word.length(); j++){
+                // 2. 단어 불일치 시 break
+                if(i+j >= doc.length() || doc.charAt(i+j) != word.charAt(j)) {
+                    matchCase = false;
+                    break;
+                }
+            }
+            if(matchCase){
+                count++;
+                i += word.length()-1;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
